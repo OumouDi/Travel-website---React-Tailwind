dee31b79971a73ce09f49e6d14796ed8f921c691
OumouDi <124316387+OumouDi@users.noreply.github.com>
2024-02-25 22:38:37 +0100

    search bar

--- my-app/src/components/Background.jsx
+++ my-app/src/components/Background.jsx
@@ -1,5 +1,5 @@
 import background from "../assets/background.mp4";
-
+import { AiOutlineSearch } from "react-icons/ai";
 
 function Background() {
   return (
@@ -15,6 +15,21 @@ function Background() {
       <div className="absolute top-0  w-full h-full flex flex-col justify-center text-center text-white p-4">
         <h1 className="mb-4">Find your dream destination !</h1>
         <p>In the heart of Africa's most beautiful landscape</p>
+
+        <form className="flex justify-between items-center max-w-[400px] mx-auto w-full border p-1 rounded-md text-black bg-gray-100 mt-6 ">
+          <div>
+            <input
+              className="bg-transparent w-[300px] sm:w-[300px] font-[Poppins] focus:outline-none"
+              type="text"
+              placeholder="search a destination..."
+            />
+          </div>
+          <div>
+            <button>
+              <AiOutlineSearch size={20} />
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
